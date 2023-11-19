fc0e04dc70786e632f8a2e8bdde099471c7a93f5
Filip Filmar <filmil@gmail.com>
2023-11-19 22:04:32 +0000

    feat: add a run.vivado.sh script

---
+++ run.vivado.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# A script that runs the graphical vivado tool from within the built container.
+
+set -euo pipefail
+set -x
+
+INTERACTIVE=""
+if sh -c ": >/dev/tty" >/dev/null 2>/dev/null; then
+	# Only add these if running on actual terminal.
+	INTERACTIVE="--interactive --tty"
+fi
+
+readonly VIVADO_PATH="/opt/Xilinx/Vivado/2023.2"
+
+docker run \
+  ${INTERACTIVE} \
+  -u $(id -u):$(id -g) \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -v "${PWD}:/work:rw" \
+  -e DISPLAY="${DISPLAY}" \
+  -e HOME="/work" \
+  --net=host \
+  xilinx-vivado:latest \
+  /bin/bash -c \
+    "env \
+      LD_LIBRARY_PATH=${VIVADO_PATH}/lib/lnx64.o \
+      ${VIVADO_PATH}/bin/setEnvAndRunCmd.sh vivado \
+    "
